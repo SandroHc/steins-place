f02abf60ba76c29a8905198064ad3f64f0f6ca16
rplacesuperstonk <102842881+rplacesuperstonk@users.noreply.github.com>
2022-04-02 00:53:50 -0300

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,8 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/rplacesuperstonk/rplace-image/main/superstonk_overlay.png";
+            const time = Math.floor(Date.now() / 1000);
+            i.src = "https://raw.githubusercontent.com/rplacesuperstonk/rplace-image/main/superstonk_overlay.png?tstamp=" + time;
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
