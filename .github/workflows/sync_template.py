51301c627ddc010a12ffd1fc17255255b7574bfe
Sandro Marques <sandro123iv@gmail.com>
2022-04-02 22:12:02 +0100

    Cleanup and update references to r/SteinsPlace

--- .github/workflows/sync_template.py
+++ .github/workflows/sync_template.py
@@ -1,22 +1,21 @@
-from PIL import Image, ImageOps
-import requests
 from io import BytesIO
+from PIL import Image
+import requests
 
-response = requests.get("https://raw.githubusercontent.com/rplacesuperstonk/rplace-image/main/reference.png")
+response = requests.get("https://raw.githubusercontent.com/sandrohc/r-steins-place/master/reference.png")
 img = Image.open(BytesIO(response.content))
 img = img.resize((img.size[0] * 3, img.size[1] * 3), Image.NEAREST)
 
-mask_url = "https://media.discordapp.net/attachments/267492253168173056/959625681141104700/mask.png"
+mask_url = "https://raw.githubusercontent.com/sandrohc/r-steins-place/master/mask.png"
 response = requests.get(mask_url)
 mask_i = Image.open(BytesIO(response.content))
 mask = Image.new("1", (3000, 3000), 0)
 mask.paste(mask_i)
 
-tl = (773 * 3, 735  * 3) # top left corner
+tl = (1499 * 3, 3 * 3)  # top left corner
 
-final_img = Image.new('RGBA', (3000, 3000))
 unmasked_img = Image.new('RGBA', (3000, 3000))
 unmasked_img.paste(img, tl)
+final_img = Image.new('RGBA', (3000, 3000))
 final_img = Image.composite(final_img, unmasked_img, mask)
-final_img.save("superstonk_overlay.png")
-
+final_img.save("overlay.png")
