0f505a68cc377d0119ce8d80dac826a48821bb55
Sandro Marques <sandro123iv@gmail.com>
2022-04-03 12:22:11 +0100

    Cleanup Greasemonkey script

--- overlay.js
+++ overlay.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/SteinsPlace overlay
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  Overlay for the r/SteinsPlace!
 // @author       SandroHc
 // @match        https://hot-potato.reddit.com/embed*
@@ -13,17 +13,14 @@
 const overlay = "https://raw.githubusercontent.com/sandrohc/r-steins-place/master/overlay.png?tstamp=" + Math.floor(Date.now() / 10000);
 
 if (window.top !== window.self) {
+    console.log("Placing overlay for r/SteinsPlace:", overlay);
+
+    const overlay_img = document.createElement("img");
+    overlay_img.src = overlay;
+    overlay_img.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+
     window.addEventListener('load', () => {
         const canvas = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0];
-        canvas.appendChild(
-            (function () {
-                const i = document.createElement("img");
-                i.id = "custom-overlay";
-                i.src = overlay;
-                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
-                console.log("Placing r/SteinsPlace overlay from:", i.src);
-                return i;
-            })()
-        )
+        canvas.appendChild(overlay_img);
     }, false);
 }
