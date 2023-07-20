7e2c79736314d291654d7035422b911bf52c42a8
Sandro Marques <sandro123iv@gmail.com>
2023-07-20 15:39:22 +0100

    Updated Greasemonkey script

--- overlay.js
+++ overlay.js
@@ -1,26 +1,32 @@
 // ==UserScript==
-// @name         r/SteinsPlace overlay
+// @name         Steins;Place 2023
 // @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  Overlay for the r/SteinsPlace!
+// @version      2
+// @description  Steins;Place overlay for r/place 2023.
 // @author       SandroHc
-// @match        https://hot-potato.reddit.com/embed*
+// @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
 const overlay = "https://raw.githubusercontent.com/sandrohc/r-steins-place/master/overlay.png?tstamp=" + Math.floor(Date.now() / 10000);
+console.log("[PLACE] Placing overlay for Steins;Place:", overlay);
 
 if (window.top !== window.self) {
-    console.log("Placing overlay for r/SteinsPlace:", overlay);
-
     const overlay_img = document.createElement("img");
     overlay_img.src = overlay;
-    overlay_img.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+    overlay_img.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
 
     window.addEventListener('load', () => {
-        const canvas = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0];
+        const canvas = document.getElementsByTagName('garlic-bread-embed')[0]
+            .shadowRoot.children[0]
+            .getElementsByTagName('garlic-bread-share-container')[0]
+            .getElementsByTagName('garlic-bread-camera')[0]
+            .getElementsByTagName('garlic-bread-canvas')[0]
+            .shadowRoot.children[0];
+
+        console.log("[PLACE] Placing on canvas:", canvas);
         canvas.appendChild(overlay_img);
     }, false);
 }
