af8f056be09f9819fc42c331ca9b325327d3da1a
Sandro Marques <sandro123iv@gmail.com>
2023-07-20 16:43:41 +0100

    Update overlay

--- sync_overlay.py
+++ sync_overlay.py
@@ -5,22 +5,15 @@ canvas_width = 2000
 canvas_height = 2000
 canvas_scale = 3
 
-print('Preparing reference-starter.png')
-starter_pos = (0 * canvas_scale, 0 * canvas_scale)  # top left corner
-starter_img = open("reference-starter.png", "rb").read()
-starter_img = Image.open(BytesIO(starter_img))
-starter_img = starter_img.resize((starter_img.size[0] * canvas_scale, starter_img.size[1] * canvas_scale), Image.Resampling.NEAREST)
-
-print('Preparing reference-titanfolk.png')
-titanfolk_pos = (100 * canvas_scale, 641 * canvas_scale)  # top left corner
-titanfolk_img = open("reference-titanfolk.png", "rb").read()
-titanfolk_img = Image.open(BytesIO(titanfolk_img))
-titanfolk_img = titanfolk_img.resize((titanfolk_img.size[0] * canvas_scale, titanfolk_img.size[1] * canvas_scale), Image.Resampling.NEAREST)
+print('Preparing reference-simple.png')
+simple_pos = (248 * canvas_scale, 709 * canvas_scale)  # top left corner
+simple_img = open("reference-simple.png", "rb").read()
+simple_img = Image.open(BytesIO(simple_img))
+simple_img = simple_img.resize((simple_img.size[0] * canvas_scale, simple_img.size[1] * canvas_scale), Image.Resampling.NEAREST)
 
 print('Joining all references')
 unmasked_img = Image.new('RGBA', (canvas_width * canvas_scale, canvas_height * canvas_scale))
-# unmasked_img.paste(starter_img, starter_pos)
-unmasked_img.paste(titanfolk_img, titanfolk_pos)
+unmasked_img.paste(simple_img, simple_pos)
 
 print('Preparing mask')
 mask_img = open("mask.png", "rb").read()
